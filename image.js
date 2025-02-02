51bf9cb2be8f3a20adf7aeedaca97030ddc03d84
DivyanshuGiri007 <bca2326062@gmail.com>
2025-02-02 18:47:42 +0530

    Add files via upload

---
+++ image.js
@@ -0,0 +1,13 @@
+var fullimgBox = document.getElementById("fullimgBox");
+var fullimg = document.getElementById("fullimg");
+
+function openfullimg(pic){
+    fullimgBox.style.display="flex"
+    fullimg.src=pic;
+
+};
+
+function closefullimg(){
+    fullimgBox.style.display="none"
+
+};
